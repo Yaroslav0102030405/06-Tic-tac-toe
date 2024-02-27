f01b23d404bb4f7f3ba5eede790e197a5beaf2b7
Yaroslav Lukovetskyi <lukovetskyi@gmail.com>
2024-02-27 21:05:04 +0200

    fix

---
+++ loader.js
@@ -0,0 +1,18 @@
+// const percents = document.querySelector(".preloader__percents");
+// const preloader = document.querySelector(".preloader");
+
+// for (let i = 0; i <= 100; i += 1) {
+//   percents.innerHTML = i;
+//   //   console.log(i);
+//   //   percents.innerHTML = (i * 100) / i;
+//   //   console.log(i);
+//   //   console.log(percents.innerHTML);
+//   //   if (i === ) {
+//   //     // percents.classList.add('')
+//   //     percents.innerHTML = 100;
+//   //   }
+
+//   if (i === 100) {
+//     preloader.classList.add("none-active");
+//   }
+// }
